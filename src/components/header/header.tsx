08b239c502a0e3f7bc35ef17139a624000d60540
Harshit <harshit.owo@gmail.com>
2023-11-17 12:19:44 +0530

    fixed ham

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,13 @@
 import { $, component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
 import Logo from "../logo";
 import HeaderStyles from "./style.css?inline"
+import HamStyles from "./ham.css?inline"
 import Accordion from "../accordion/accordion";
 import AccordionItem from "../accordion/accordion-item/accordion-item";
 
 export default component$(() => {
 useStylesScoped$(HeaderStyles)
+useStylesScoped$(HamStyles)
   const hamVisible = useSignal(false)
   const toggleHam =  $( () => {
     hamVisible.value = !hamVisible.value
@@ -18,11 +20,17 @@ useStylesScoped$(HeaderStyles)
           <a href="/">
           <Logo/>
           </a>
-          
+          <svg  class={`ham hamRotate ham7 ${hamVisible.value && "active"}`} onClick$={toggleHam}  viewBox="0 0 100 100" width="80" onclick="this.classList.toggle('active')">
+          <path class="line top" d="m 70,33 h -40 c 0,0 -6,1.368796 -6,8.5 0,7.131204 6,8.5013 6,8.5013 l 20,-0.0013" />
+    <path class="line middle" d="m 70,50 h -40" />
+    <path class="line bottom"
+        d="m 69.575405,67.073826 h -40 c -5.592752,0 -6.873604,-9.348582 1.371031,-9.348582 8.244634,0 19.053564,21.797129 19.053564,12.274756 l 0,-40" />
+ </svg>
 
-          <button  class={`ham ${hamVisible.value && "is-active"}`} onClick$={toggleHam}>
+
+          {/* <button  class={`ham ${hamVisible.value && "is-active"}`} onClick$={toggleHam}>
             <div class="bar"></div>
-          </button>
+          </button> */}
           </nav>
          
           <aside class={` mobile-nav ${hamVisible.value && "is-active"}`} >
