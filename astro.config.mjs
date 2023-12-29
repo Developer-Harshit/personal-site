d70307da031c3eab69c39b8309d517daf044527a
Harshit <harshit.owo@gmail.com>
2023-12-29 14:33:58 +0530

    📱Initialized PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
+import AstroPWA from "@vite-pwa/astro";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    AstroPWA({
+      mode: "production",
+      base: "/",
+      scope: "/",
+
+      registerType: "autoUpdate",
+
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
+  ],
+});
