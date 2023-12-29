31cbfdffbbd5b677fcfc4630477c4a7e56e93d15
Harshit <harshit.owo@gmail.com>
2023-12-29 09:17:03 +0530

    ➕ Created landing page

---
+++ src/assets/project-data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    href: "/projects/colorify",
+    src: "template",
+    title: "Colorify",
+    tags: ["three.js", "webgl"],
+  },
+  {
+    href: "/projects/colorify",
+    src: "template",
+    title: "Colorify",
+    tags: ["three.js", "webgl"],
+  },
+  {
+    href: "/projects/colorify",
+    src: "template",
+    title: "Colorify",
+    tags: ["three.js", "webgl"],
+  },
+  {
+    href: "/projects/colorify",
+    src: "template",
+    title: "Colorify",
+    tags: ["three.js", "webgl"],
+  },
+];
