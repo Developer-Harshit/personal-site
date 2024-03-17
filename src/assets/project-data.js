a14f30da7a978bb0287c075da93a4e40eb1f8e27
Harshit <harshit.owo@gmail.com>
2024-03-17 07:05:59 +0530

    ⚡

--- src/assets/project-data.js
+++ src/assets/project-data.js
@@ -1,12 +1,12 @@
 export default [
   {
-    href: "https://",
+    href: "https://colourify.vercel.app",
     src: "colourify",
     title: "Colorify",
     tags: ["image processing"],
   },
   {
-    href: "https://",
+    href: "https://chromosketch.vercel.app",
     src: "chromosketch",
     title: "Chromosketch",
     tags: ["video effects"],
